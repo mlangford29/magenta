707799b8853ce98a2eb287638b3955f74defd2d8
Michael Langford <michael.langford29@gmail.com>
2019-02-18 18:51:22 -0600

    lengths change

--- magenta/models/shared/events_rnn_graph.py
+++ magenta/models/shared/events_rnn_graph.py
@@ -290,6 +290,7 @@ def get_build_graph_fn(mode, config, sequence_example_file_paths=None):
       ##### SHAPE IS WRONG CURRENTLY HARDCODING TO TRY TO FIND ISSUE
       # how can we grab the size we're supposed to be?
       # pretty sure this is "lengths" around line 257. Not sure if this is 1D or not
+      print(lengths)
       shape = lengths
 
       # mAY NEED TO UPDATE THIS BASED ON RNN_LAYER_SIZES?
