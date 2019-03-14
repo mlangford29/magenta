db75b457c7643fff07fdbc4c725ffc116019017d
Michael Langford <michael.langford29@gmail.com>
2019-03-14 17:44:24 -0500

    what are commas even

--- magenta/models/performance_rnn/batch_train.py
+++ magenta/models/performance_rnn/batch_train.py
@@ -18,11 +18,11 @@ generate_str = 'python performance_rnn_generate.py --config={} --run_dir=./logdi
 attn_substr = ' --hparams=\'attn_length\'=128'
 
 # all the configs you want to try
-config_list = ['simple_rnn'
+config_list = ['simple_rnn',
 				'performance_with_dynamics_and_modulo_encoding',
 				'simple_rnn', 
 				'performance_with_dynamics_and_modulo_encoding']
-config_names = ['simple_rnn'
+config_names = ['simple_rnn',
 				'lstm',
 				'simple_rnn_attn', 
 				'lstm']
