7e8799d71ae4233962f32444d614d9f22d657036
Michael Langford <michael.langford29@gmail.com>
2019-04-28 21:24:06 -0500

    final generation changes

--- magenta/models/performance_rnn/generate_optimize.py
+++ magenta/models/performance_rnn/generate_optimize.py
@@ -4,29 +4,32 @@
 
 ### imports
 import os
+from magenta.models.performance_rnn import performance_model
 ### end imports
 
 # defining string skeletons to format and execute on command line
 input_dir_str = 'INPUT_DIRECTORY=./wtc_performance'
 seq_str = 'SEQUENCES_TFRECORD=./notesequences.tfrecord'
 config_str = 'CONFIG={}'
-generate_str = 'python performance_rnn_generate.py --config={} --run_dir=./logdir/{} --output_dir=./logdir/{}/generated --num_outputs=1 --num_steps=10000 --temperature={} --beam_size={} --branch_factor={} --steps_per_iteration={}'
+generate_str = 'python performance_rnn_generate.py --config={} --run_dir=./logdir/{} --output_dir=./logdir/{}/generated --num_outputs=1 --num_steps=5000 --temperature={} --beam_size={} --branch_factor={} --steps_per_iteration={}'
 attn_substr = ' --hparams=\'attn_length\'=128'
 
 # all the configs you want to try
 
 ##### WE NEED SOME MORE OF THESE
-config_list = ['performance_with_dynamics_and_modulo_encoding']#,
+config_list = ['simple_rnn']#,
 				#'performance_with_dynamics_and_modulo_encoding']
-config_names = ['lstm']
+config_names = ['simple_rnn']
 #####
 
 # generation parameter lists for a grid search
-t_list = [0.9]
-bs_list = [16]
-bf_list = [16]
-spi_list = [32]
-num_to_gen = 20
+# instead of list for each, we just have a list for the starting points
+# it will be in order of t, bs, bf, spi
+#t_list = [0.9]
+#bs_list = [16]
+#bf_list = [16]
+#spi_list = [32]
+x0 = [0.9, 16, 16, 32]
 
 # random starting triad!
 def make_starting_triad():
@@ -91,32 +94,30 @@ def add_primer(gen_str):
 
 	return gen_str
 
+# the actual function to minimize!
+def logloss(x):
+
+	# currently only doing the first one cause duh
+	config = config_list[0]
+	config_name = config_names[0]
+
+	_generate_str = generate_str.format(config, config_name, config_name, x[0], x[1], x[2], x[3])
+	gen_str = add_primer(_generate_str)
+
+	if 'attn' in config_name:
+		gen_str += attn_substr
+
+	os.system(gen_str)
+
+	# you need to find a way to grab the output file and call performance_log_likelihood on it
+	
+
 
 # execute the first definitions
 os.system(input_dir_str)
 os.system(seq_str)
 
-for i in range(len(config_list)):
 
-	config = config_list[i]
-	config_name = config_names[i]
-
-	# grid search!!
-	for t in t_list:
-		for bs in bs_list:
-			for bf in bf_list:
-				for spi in spi_list:
-
-					for i in range(num_to_gen):
-						
-						_generate_str = generate_str.format(config, config_name, config_name, t, bs, bf, spi)
-
-						gen_str = add_primer(_generate_str)
-
-						if 'attn' in config_name:
-							gen_str += attn_substr
-
-						os.system(gen_str)
 
 print('Complete!')
 
